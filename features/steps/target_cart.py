75d2d8be0c3c82765e0869d493caa35bf8525f38
sumana vittal <v.sumana@gmail.com>
2023-11-05 20:20:42 -0800

    features/steps/target_cart.py
    : rearrange code

--- features/steps/target_cart.py
+++ features/steps/target_cart.py
@@ -14,29 +14,6 @@ def click_on_cart(context):
     sleep(2)
 
 
-@then('Verify "Your cart is empty" message is displayed')
-def verify_empty_cart_message(context):
-    result = context.driver.find_element(By.XPATH, "//h1[text()='Your cart is empty']").text
-    assert result == 'Your cart is empty', f"Error: Expected 'Your cart is empty' but got {result}"
-
-
-@when('Click on Sign In')
-def click_on_sign_in(context):
-    context.driver.find_element(By.XPATH, "//a[@data-test='@web/AccountLink']").click()
-
-
-@when('From right side navigation menu, click Sign In')
-def click_menu_sign_in(context):
-    context.driver.find_element(By.XPATH, "//a[@data-test='accountNav-signIn']").click()
-
-
-@then('Verify Sign In form opened')
-def verify_sign_in_opened(context):
-    result_sign_in_button = context.driver.find_element(By.ID, "login").is_displayed()
-    result_user_field = context.driver.find_element(By.ID, "username").is_displayed()
-    assert (result_sign_in_button and result_user_field) is True, f"Error: Expected the Sign In page!"
-
-
 @when('Search the product to add')
 def search_product(context):
     context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/Search/SearchInput']").send_keys('mug')
@@ -60,5 +37,4 @@ def verify_cart_item(context):
     result_text = (context.driver.find_element(By.CSS_SELECTOR, "span[class*='styles__CartSummarySpan']").
                    text)
     assert expected_text in result_text, f"Error: Expected text {expected_text} is not {result_text}."
-
 
--- features/tests/target_cart.feature
+++ features/tests/target_cart.feature
@@ -1,15 +1,9 @@
-Feature: Testing Target.com features
+Feature: Testing cart features
 
   Scenario: User check for empty cart message
     Given Open target home page
     When  Click on the Cart icon
     Then  Verify "Your cart is empty" message is displayed
-
-  Scenario: User check Logged out user can access Sign In
-    Given Open target home page
-    When  Click on Sign In
-    And   From right side navigation menu, click Sign In
-    Then  Verify Sign In form opened
 
   Scenario: User check the added product to the cart
     Given Open target home page
